08a172f5cc252e69314aa3a36e91ddaac709f55f
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-02-14 12:14:36 +0300

    Update included SPA project module

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ main.time;
 import { default as Library } from './modules/mainprog.js';
 
 import { myLib } from './modules/newInstance.js';
+
+import * as spa from './modules/spa.js';
+spa.contactBtn;
+spa.listBtn;
+spa.section1;
+spa.section2;
+spa.section3;
+spa.addNewBtn;
+spa.contactShow();
+spa.listShow();
+spa.addNewShow();
